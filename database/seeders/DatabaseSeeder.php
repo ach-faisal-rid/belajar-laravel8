14ded2564c181bfeae17f7b6a2304561ce4bea0f
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2023-08-24 11:40:18 +0700

    membuat seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,39 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        User::create([
+            'name' => "Faisal Ridhani",
+            'email' => "faisalachmad.ff@gmail.com",
+            'password' => bcrypt('123456')
+        ]);
+        Category::create([
+            'name' => "Web Programming",
+            'slug' => "web-programming"
+        ]);
+        Category::create([
+            'name' => "Personal",
+            'slug' => "personal"
+        ]);
+        Post::create([
+            'title' => "Judul Pertama",
+            'slug' => "judul-pertama",
+            'excerpt' => "lorem impsn",
+            'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
+            excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+            quod voluptate. Omnis, dolor",
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => "Judul Kedua",
+            'slug' => "judul-kedua",
+            'excerpt' => "lorem impsn",
+            'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
+            excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+            quod voluptate. Omnis, dolor",
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
     }
 }
