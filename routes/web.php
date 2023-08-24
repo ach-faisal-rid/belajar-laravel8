510019a24a84365efa5b274e322665c6904287ea
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2023-08-24 13:51:36 +0700

    penambahan eager loading dan lazy eager loading

--- routes/web.php
+++ routes/web.php
@@ -35,14 +35,14 @@ Route::get('/categories', function(){
 Route::get('/categories/{category:slug}', function(Category $category) {
     return view('posts', [
         'title' => "Post by Category : $category->name",
-        'posts' => $category->posts,
+        'posts' => $category->posts->load('author', 'category'),
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
         'title' => "Posts by author : $author->name",
-        'posts' => $author->posts,
+        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
 Route::get('/welcome', function () {
