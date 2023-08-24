00c756eda7d40e1a531c41f000bd2065475cf2ce
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2023-08-24 18:56:11 +0700

    membuat post image unsplash api

--- resources/views/Posts.blade.php
+++ resources/views/Posts.blade.php
@@ -1,6 +1,25 @@
 @extends('template')
 @section('container')
     <h1>apa yang baru ? {{ $title }} </h1>
+    @if ($posts->count())
+    <div class="card mb-3">
+        <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h3 class="card-title">{{ $posts[0]->title }}</h3>
+            <small class="text-muted">
+            <p>by. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in
+            <a href="/categories/{{ $posts[0]->category->slug }}"
+            class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+            {{ $posts[0]->created_at->diffForHumans() }}</small>
+            </p>
+            <p class="card-text">{{ $posts[0]->excerpt }}</p>
+            <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">read more</a>
+        </div>
+    </div>
+    @else
+        <p class="text-center fs-4">no post found.</p>
+    @endif
+
     @foreach ($posts as $post)
         <article class="mb-5 border-bottom pb-5">
             <h2 class="mb-5">
