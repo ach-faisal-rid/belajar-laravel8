aea1dcbc493d0361a989ef76204ea79db85b8023
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2023-08-24 13:43:16 +0700

    terjadi n+1 problem menginstall clockwork

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
         return view('posts', [
             "title" => "all Post",
             // "posts" => Post::all()
-            "posts" => Post::latest()->get()
+            "posts" => Post::with(['author', 'category'])->latest()->get()
         ]);
     }
 
