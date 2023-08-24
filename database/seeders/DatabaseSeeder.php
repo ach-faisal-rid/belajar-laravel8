4bc0eb0ece4a15880e483e5895fd5a85ae37bb70
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2023-08-24 12:33:04 +0700

    membuat factory untuk post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,17 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
-        User::create([
-            'name' => "Faisal Ridhani",
-            'email' => "faisalachmad.ff@gmail.com",
-            'password' => bcrypt('123456')
-        ]);
-        User::create([
-            'name' => "Teguh firman gautama",
-            'email' => "teguh.fg@gmail.com",
-            'password' => bcrypt('123456')
-        ]);
+        User::factory(10)->create();
+        // User::create([
+        //     'name' => "Faisal Ridhani",
+        //     'email' => "faisalachmad.ff@gmail.com",
+        //     'password' => bcrypt('123456')
+        // ]);
+        // User::create([
+        //     'name' => "Teguh firman gautama",
+        //     'email' => "teguh.fg@gmail.com",
+        //     'password' => bcrypt('123456')
+        // ]);
         Category::create([
             'name' => "Web Programming",
             'slug' => "web-programming"
@@ -35,46 +35,48 @@ class DatabaseSeeder extends Seeder
             'name' => "Personal",
             'slug' => "personal"
         ]);
-        Post::create([
-            'title' => "Judul Pertama",
-            'slug' => "judul-pertama",
-            'excerpt' => "lorem impsn",
-            'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
-            excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
-            quod voluptate. Omnis, dolor",
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
-        Post::create([
-            'title' => "Judul Kedua",
-            'slug' => "judul-kedua",
-            'excerpt' => "lorem impsn",
-            'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
-            excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
-            quod voluptate. Omnis, dolor",
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
-        Post::create([
-            'title' => "Judul Ketiga",
-            'slug' => "judul-ke-tiga",
-            'excerpt' => "lorem impsn",
-            'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
-            excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
-            quod voluptate. Omnis, dolor",
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
-        Post::create([
-            'title' => "Judul keempat",
-            'slug' => "judul-ke-empat",
-            'excerpt' => "lorem impsn",
-            'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
-            excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
-            quod voluptate. Omnis, dolor",
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+
+        Post::factory(20)->create();
+        // Post::create([
+        //     'title' => "Judul Pertama",
+        //     'slug' => "judul-pertama",
+        //     'excerpt' => "lorem impsn",
+        //     'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
+        //     excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+        //     quod voluptate. Omnis, dolor",
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => "Judul Kedua",
+        //     'slug' => "judul-kedua",
+        //     'excerpt' => "lorem impsn",
+        //     'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
+        //     excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+        //     quod voluptate. Omnis, dolor",
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => "Judul Ketiga",
+        //     'slug' => "judul-ke-tiga",
+        //     'excerpt' => "lorem impsn",
+        //     'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
+        //     excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+        //     quod voluptate. Omnis, dolor",
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
+        // Post::create([
+        //     'title' => "Judul keempat",
+        //     'slug' => "judul-ke-empat",
+        //     'excerpt' => "lorem impsn",
+        //     'body' => "enim at in obcaecati tempora beatae laudantium voluptate aperiam
+        //     excepturi delectus recusandae nemo aliquam deleniti temporibus itaque? Modi,
+        //     quod voluptate. Omnis, dolor",
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
 
     }
 }
